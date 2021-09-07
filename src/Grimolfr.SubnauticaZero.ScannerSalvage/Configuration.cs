9c4bc5ac7f35d7999f92d584b5e9b4f9d1422838
GrimGadget <30664204+GrimGadget@users.noreply.github.com>
2021-09-07 15:16:15 -0400

    Starting work on Scanner Salvage mod

---
+++ src/Grimolfr.SubnauticaZero.ScannerSalvage/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+
+namespace Grimolfr.SubnauticaZero.ScannerSalvage
+{
+    [Menu(
+        Main.ModName,
+        MemberProcessing = MenuAttribute.Members.Explicit,
+        LoadOn = MenuAttribute.LoadEvents.MenuOpened,
+        SaveOn = MenuAttribute.SaveEvents.ChangeValue)]
+    internal class Configuration
+        : ConfigFile
+    {
+        [Toggle(Label = "Enable", Tooltip = "Check to enable this mod.", Order = 0)]
+        public bool IsEnabled = true;
+    }
+}
---
+++ src/Grimolfr.SubnauticaZero.ScannerSalvage/Main.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+
+namespace Grimolfr.SubnauticaZero.ScannerSalvage
+{
+    [QModCore]
+    public static class Main
+    {
+        internal const string ModName = "Scanner Salvage";
+
+        internal static Configuration Config { get; private set; }
+
+        [QModPatch]
+        public static void Initialize()
+        {
+            Log.Info("Registering Scanner Salvage configuration...");
+            Config = OptionsPanelHandler.Main.RegisterModOptions<Configuration>();
+            if (!File.Exists(Config.JsonFilePath))
+                Config.Save();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = assembly.GetName().Name;
+
+            Log.Info($"Processing patches for {assemblyName}...");
+            var harmony = new Harmony($"Grimolfr_{assemblyName}");
+            harmony.PatchAll(assembly);
+        }
+    }
+}
