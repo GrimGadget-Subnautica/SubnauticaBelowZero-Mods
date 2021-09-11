dbd4bd8dfab0e2a9d0bfb1211a7c509257251e53
GrimGadget <30664204+GrimGadget@users.noreply.github.com>
2021-09-11 10:45:13 -0400

    Tweak/fix for auto-calculated weighting algorithm

--- src/Grimolfr.SubnauticaZero.SalvageScanning/Salvage/SalvageItem.cs
+++ src/Grimolfr.SubnauticaZero.SalvageScanning/Salvage/SalvageItem.cs
@@ -9,6 +9,6 @@ namespace Grimolfr.SubnauticaZero.SalvageScanning.Salvage
         public int Amount { get; set; }
         public int MaxDepth { get; set; }
 
-        public double Weight => 1.0 / (Rarity + 2^Math.Min(8 - MaxDepth, 0));
+        public double Weight => 1.0 / (Rarity * 2^Math.Max(4 - MaxDepth, 0));
     }
 }
